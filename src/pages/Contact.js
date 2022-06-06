926af0afc54c3439626ad5ad56e451d02e3f6700
Sargamdev <sargamtmg917@gmail.com>
2022-06-06 08:12:40 +0545

    Animation of envelop in contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,12 @@
 import React, { useRef } from "react";
-import emailjs from '@emailjs/browser'
+import emailjs from '@emailjs/browser';
+import openEnvelop from '../img/open_envelop.png';
 
 const Contacts = () => {
 
     const form = useRef(null);
 
-    const sendEmail = (e) => {
+    const sendEmail = async (e) => {
         e.preventDefault();
     
         emailjs.sendForm('service_l1y7h1j', 'template_levulc7', form.current, 'dlG0vLmiqkwJtfzi5')
@@ -14,22 +15,69 @@ const Contacts = () => {
         }, (error) => {
             console.log(error.text);
         });
+
+        console.log(form.current);
+        form.current.reset();
+        formInEnvelop();
+        closeEnvAnimate();
+        adjustEnvPos();
     };
+
+    //animation for putting form in envelop
+    function formInEnvelop() {
+        let form_letter = document.querySelector('.feedback_form');
+        form_letter.style.transform = 'translateY('+50+'vw) rotateZ(0deg)';
+    }
+
+    //animation for closing envelop
+    function closeEnvAnimate() {
+        let id = null;
+        const elem = document.querySelector('.env-top-total');
+        let angle = 0;
+        clearInterval(id);
+        id = setInterval(frame, 20);
+        function frame() {
+            if (angle === 180) {
+                clearInterval(id);
+            } else {
+                angle = angle+5; 
+                elem.style.transform = 'perspective(500px) rotateX(-'+angle+'deg)';
+                if(angle > 90)
+                {
+                    elem.style.zIndex = 20;
+                }
+            }
+        }
+    }
+
+    function adjustEnvPos (){
+        let env = document.querySelector('.envelop');
+        env.style.transform = 'translateY(-50vw)';
+    }
 
     return(
         <>
         <h1>Contact page</h1>
-        <form ref={form}>
-            <label>Your Name : </label>
-            <input type="text" name='user_name'></input>
-            <label>Email : </label>
-            <input type="text" name='user_email'></input>
-            <label>Tittle : </label>
-            <input type="text" name='user_tittle'></input>
-            <label>message:</label>
-            <textarea name='user_message'></textarea>
-            <button onClick={sendEmail}> Send </button>
-        </form>
+        <div className='envelop'>
+                <form ref={form} className='feedback_form'>
+                    <label>Your Name : </label>
+                    <input type="text" name='user_name'></input>
+                    <label>Email : </label>
+                    <input type="text" name='user_email'></input>
+                    <label>Tittle : </label>
+                    <input type="text" name='user_tittle'></input>
+                    <label>message:</label>
+                    <textarea name='user_message'></textarea>
+                    <button onClick={sendEmail}> Send </button>
+                </form>
+                <div className="env-top-total">
+                    <div className="env-top">
+                    </div>
+                    <div className="env-top-invisible"></div>
+                </div>
+                <div className="env-back"></div>
+                <img src={openEnvelop} alt='open envelop' className='open-envelop'></img>
+        </div>
         </>
     );
 }
