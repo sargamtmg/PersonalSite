def3ca57ca14988b0dafbf25579c8073bcdf2f41
sargamdev2-at-679298521299 <sargamdev2-at-679298521299>
2023-07-08 16:50:46 +0545

    Changes in Homepage and better arrangement

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,4 +1,6 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect} from 'react';
+import {gsap} from 'gsap';
+import ScrollTrigger from "gsap/ScrollTrigger";
 import { Link } from "react-router-dom";
 import sletterTop from '../img/sletterTop.png';
 import sletterMiddle from '../img/sletterMiddle.png';
@@ -9,6 +11,170 @@ const Homepage = () =>{
     let myref2 = useRef(null);
     let myref3 = useRef(null);
 
+    gsap.registerPlugin(ScrollTrigger);
+
+    //this is to be used
+
+    useEffect(()=>{
+        console.log('useEffect on main page');
+        let ctx = gsap.context(()=>{
+
+            //initial setting height for letterT
+            gsap.from('.letterT',{
+                height: '80vh'
+            });
+
+            //elongate the upperline of T
+            gsap.to('.upperline',{
+                width: '90vw',
+                scrollTrigger:{
+                    trigger: '.purple',
+                    start: 'bottom 100%',
+                    end: 'bottom 50%',
+                    //markers:true,
+                    scrub: true,
+                    toggleActions: 'play pause reverse none',
+                }
+            });
+
+            //decrease size of T
+            gsap.to('.letterT',{
+                height: '45vh',
+                duration: 4,
+                delay: 2,
+                scrollTrigger:{
+                    trigger: '.purple',
+                    start: 'bottom 50%',
+                    end: 'bottom 10%',
+                    //markers:true,
+                    scrub: true,
+                    toggleActions: 'play pause reverse none',
+                }
+            });
+
+            //overlap the upper two mini
+            gsap.to('.aboutMe_mini , .project_mini',{
+                x:-20,
+                y: 170,
+                duration: 1,
+                scrollTrigger:{
+                    trigger: '.purple',
+                    start: 'bottom 50%',
+                    end: 'bottom 10%',
+                    //markers:true,
+                    scrub: true,
+                    toggleActions: 'play pause reverse none',
+                }
+            });
+
+            //width 0 to minis
+            gsap.to('.mini_collection',{
+                width: 0,
+                height: '20vh',
+                duration: 1,
+                scrollTrigger:{
+                    trigger: '.purple',
+                    start: 'bottom 10%',
+                    end: 'bottom -35%',
+                    //markers:true,
+                    scrub: true,
+                    toggleActions: 'play pause reverse none',
+                }
+            });
+
+            //decrease size of T upperline
+            gsap.fromTo('.upperline',{width:'90vw'},{
+                width: '15vw',
+                minWidth: '90px',
+                height:'20px',
+                duration: 1,
+                scrollTrigger:{
+                    trigger: '.purple',
+                    start: 'bottom 10%',
+                    end: 'bottom -35%',
+                    //markers:true,
+                    scrub: true,
+                    toggleActions: 'play pause reverse none',
+                }
+            });
+
+            //decrease size of T middle line
+            gsap.fromTo('.letterT',{height:'45vh'},{
+                height: '20vw',
+                width: '3vw',
+                duration: 4,
+                delay: 2,
+                scrollTrigger:{
+                    trigger: '.purple',
+                    start: 'bottom -35%',
+                    end: 'bottom -70%',
+                    //markers:true,
+                    scrub: true,
+                    toggleActions: 'play pause reverse none',
+                }
+            });
+
+            //just to pin purple element
+            ScrollTrigger.create({
+                trigger: '.purple',
+                start: 'bottom 100%',
+                end: 'bottom -50%',
+                //markers: true,
+                pin: true
+            });
+            
+            gsap.to('.letterT',{
+                y:'68vh',  //based on observation to align with amang at footer
+                scrollTrigger:{
+                    trigger: '.footer',
+                    start: 'top 90%',
+                    end: 'top 60%',
+                    //markers:true,
+                    scrub: true,
+                    toggleActions: 'play pause reverse none',
+                }
+            });
+
+        });
+        return () => ctx.revert();
+    },[]);
+
+    window.addEventListener('scroll', ()=>{
+        //if(!myref.current || !myref2.current || !myref3.current) return;
+        let value = window.scrollY;
+        const rede = document.querySelector('#redid');
+        let red = rede.getBoundingClientRect();
+        const transe = document.querySelector('#transid');
+        const yellowe = document.querySelector('#yellowid');
+        let yellow = yellowe.getBoundingClientRect();
+        const purplee = document.querySelector('.purple');
+        let purple = purplee.getBoundingClientRect();
+        const navChild = document.querySelector('.navChild');
+        if(red.bottom > 0 )
+        {
+            yellowe.classList.add('fixedToTop');
+            transe.classList.remove('hide');
+            navChild.classList.remove('stickyToTop');
+            if(red.bottom <= 75){
+                navChild.classList.add('stickyToTop');
+                yellowe.classList.remove('fixedToTop');
+                transe.classList.add('hide');
+            }
+        }
+        else if(red.bottom <= 0){
+            navChild.classList.add('stickyToTop');
+            yellowe.classList.remove('fixedToTop');
+            transe.classList.add('hide');
+        }
+        //page hit bottom
+        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+            let leftname = document.querySelectorAll('.leftname');
+            for (let i = 0; i < leftname.length; i++) {
+                leftname[i].classList.remove('hide');
+            }
+        }
+    });
+
     return(
         <>
         <div className='view1'>
@@ -16,11 +182,7 @@ const Homepage = () =>{
                 </div>
                 <div className='yellow layer fixedToTop' id='yellowid' ref={myref2}>
                     I am YELLOWs
-                    <div>Essay topics in English can be difficult to come up with. While writing essays, many college and high school students face writer’s block and have a hard time to think about topics and ideas for an essay. In this article, we will list out many good essay topics from different categories like argumentative essays, essays on technology, environment essays for students from 5th, 6th, 7th, 8th grades. Following list of essay topics are for all – from kids to college students. We have the largest collection of essays. An essay is nothing but a piece of content which is written from the perception of writer or author. Essays are similar to a story, pamphlet, thesis, etc. The best thing about Essay is you can use any type of language – formal or informal. It can biography, the autobiography of anyone. Following is a great list of 100 essay topics. We will be adding 400 more soon!
-
-                    But Before that you may wanna read some awesome Essay Writing Tips here.
-                    500+ essay topics for students and children
-                    Get the Huge list of 100+ Speech Topics here
+                    <div>Essay topics in
 
                     </div>
                 </div>
@@ -49,9 +211,9 @@ const Homepage = () =>{
                     <div className='myName'>
                         <div className='sargam'>
                             <div className='Sletter'>
-                                <img src={sletterTop} alt="S letter top" className='STop' width="100px" />
-                                <img src={sletterMiddle} alt="S letter middle" className='SMiddle' width="100px"/>
-                                <img src={sletterBottom} alt="BS letter Bottom" className='SBottom' width="100px"/>
+                                <img src={sletterTop} alt="S letter top" className='STop' />
+                                <img src={sletterMiddle} alt="S letter middle" className='SMiddle'/>
+                                <img src={sletterBottom} alt="BS letter Bottom" className='SBottom'/>
                             </div>
                             <div className='leftname hide'>argam</div>
                         </div>
@@ -66,7 +228,7 @@ const Homepage = () =>{
                             <a href ='#' target='_blank' className='insta handles'></a>
                             <a href='https://github.com/sargamtmg' target='_blank' className='git handles'></a>
                         </div>
-                        <div className='email_address'>sargamtmg228@gmail.ccom</div>
+                        <div className='email_address'>sargamtmg228@gmail.com</div>
                     </div>
                 </div>
             </div>
