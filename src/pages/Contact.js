a5b372673936fd515c3d1a912e33e9a35dac54b9
sargamdev2-at-679298521299 <sargamdev2-at-679298521299>
2023-07-25 20:18:44 +0545

    Finsl changes responsive contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,10 @@
 import React, { useRef } from "react";
 import emailjs from '@emailjs/browser';
-import openEnvelop from '../img/open_envelop_final.png';
-import topEnvelop from '../img/envelop_top_Final2.png';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faPaperPlane} from "@fortawesome/free-solid-svg-icons"
+import { gsap } from "gsap";
+import openEnvelop from '../img/contact/open_envelop_final.png';
+import topEnvelop from '../img/contact/envelop_top_Final.png';
 
 const Contacts = () => {
 
@@ -10,71 +13,79 @@ const Contacts = () => {
     const sendEmail = async (e) => {
         e.preventDefault();
     
-        // emailjs.sendForm('service_l1y7h1j', 'template_levulc7', form.current, 'dlG0vLmiqkwJtfzi5')
-        // .then((result) => {
-        //     console.log(result.text);
-        // }, (error) => {
-        //     console.log(error.text);
-        // });
+        emailjs.sendForm('service_l1y7h1j', 'template_levulc7', form.current, 'dlG0vLmiqkwJtfzi5')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
 
         console.log(form.current);
-        form.current.reset();
-        formInEnvelop();
         closeEnvAnimate();
-        adjustEnvPos();
     };
 
-    //animation for putting form in envelop
-    function formInEnvelop() {
-        let form_letter = document.querySelector('.feedback_form');
-        form_letter.style.transform = 'translateY('+45+'vh) rotateZ(0deg)';
+    const resetTheForm = () =>{
+        form.current.reset();
     }
 
-    //animation for closing envelop
     function closeEnvAnimate() {
-        let id = null;
-        const elem = document.querySelector('.env-top-total');
-        let angle = 0;
-        clearInterval(id);
-        id = setInterval(frame, 20);
-        function frame() {
-            if (angle === 180) {
-                clearInterval(id);
-            } else {
-                angle = angle+5; 
-                elem.style.transform = 'perspective(500px) rotateX(-'+angle+'deg)';
-                if(angle > 90)
-                {
-                    elem.style.zIndex = 20;
-                }
+        console.log('closing animation');
+        const tl = gsap.timeline();
+
+        tl.to('.envelop', {
+            duration: 0.4,
+            top:'10%',
+            ease: "power2.in",
+        })
+        .to('.feedback_form', {
+            duration: 0.8,
+            top:'0',
+            ease: "power2.out",
+        })
+        .to('.env_top_side', {
+            duration: 0.5,
+            zIndex:5,
+            perspective: 500,
+            rotationX: 180,
+            transformOrigin: 'center bottom',
+            ease: 'power2.out', // Changed from 'transition' to 'ease'
+            onComplete: ()=>{
+                resetTheForm();
             }
-        }
+        })
+        .to('.envelop', {
+            duration: 1,
+            perspective: 500,
+            translateY:'200%',
+            ease: 'power2.out', // Changed from 'transition' to 'ease'
+            onComplete: ()=>{
+                tl.reverse();
+            }
+        });
     }
 
-    function adjustEnvPos (){
-        let env = document.querySelector('.envelop');
-        env.style.transform = 'translateY(-30vh)';
-    }
 
     return(
         <>
-        <h1>Contact page</h1>
-        <div className='envelop'>
-                <div className="env-top-total">
-                    <img src={topEnvelop} alt='open envelop' className='env-top'></img>
-                    <div className="env-top-invisible"></div>
+        <div className='envelop_wrapper'>
+            <div className="envelop">
+                <img src={topEnvelop} alt='envelop top' className='env_top_side' />
+                <div className="env_body">
+                    <form ref={form} className='feedback_form'>
+                        <input type="text" name='user_name' className="user_name" placeholder="Full name" autoComplete="off" required></input>
+                        <input type="text" name='user_email' className="user_email" placeholder="E-mail" autoComplete="off" required></input>
+                        <div className="subject">
+                            <label className="subject_label">Subject: </label>
+                            <input type="text" name='user_tittle' className="user_subject" autoComplete="off"></input>
+                        </div>
+                        <textarea name='user_message' className="user_message" placeholder="Message Here ..." required></textarea>
+                        <button onClick={sendEmail} className="send_button"> Send <FontAwesomeIcon icon={faPaperPlane}/> </button>
+                    </form>
+                    <div className="envelop_bg"></div>
+                    <img src={openEnvelop} alt='open envelop' className='open_envelop'></img>
                 </div>
-                <form ref={form} className='feedback_form'>
-                    Your Name : &nbsp;<t></t><input type="text" name='user_name'></input><br></br>
-                    Email : &nbsp;&nbsp;<input type="text" name='user_email'></input><br></br>
-                    <label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Tittle : </label>
-                    <input type="text" name='user_tittle'></input><br></br>
-                    <label>message:</label>
-                    <textarea name='user_message'></textarea>
-                    <button onClick={sendEmail}> Send </button>
-                </form>
-                <div className="env-back"></div>
-                <img src={openEnvelop} alt='open envelop' className='open-envelop'></img>
+                
+            </div>
         </div>
         </>
     );
